d85456a683e84ff421af882337e440229d197c9f
vljukap98 <luka.jakovic@serengetitech.com>
2023-12-13 17:23:03 +0100

    project init

---
+++ simple-budgeting-api/src/test/java/ljakovic/simplebudgeting/SimpleBudgetingApplicationTests.java
@@ -0,0 +1,13 @@
+package ljakovic.simplebudgeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBudgetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
