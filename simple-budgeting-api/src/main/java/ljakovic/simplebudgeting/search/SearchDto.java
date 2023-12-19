9333f311a572625973a71f78d8e27d3afa7dbeca
vljukap98 <luka.jakovic@serengetitech.com>
2023-12-19 22:38:08 +0100

    remove obsolete search class, add postman collections

--- simple-budgeting-api/src/main/java/ljakovic/simplebudgeting/search/SearchDto.java
+++ simple-budgeting-api/src/main/java/ljakovic/simplebudgeting/search/SearchDto.java
@@ -6,15 +6,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SearchDto {
-    private List<SearchCriteria> criteria;
     private String sortProperty;
     private SortDirection sortDirection;
 }
